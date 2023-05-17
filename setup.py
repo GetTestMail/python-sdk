4265e6a48004502acd4cf6bec86a45dbb3dea8ef
Bobby Donchev <contact@donchev.is>
2023-05-17 15:06:27 +0200

    bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="gettestmail",
-    version="1.0.0",
+    version="1.0.1",
     author="Bobby Donchev",
     author_email="support@gettestmail.com",
     description="A Python client library for the GetTestMail API",
